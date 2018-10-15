b3c07cc8457e33af51712b119145dd6d5868476b
Robert Gerlach <info@robertgerlach.net>
2018-10-15 18:55:26 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd src
+echo "Creating Chrome extension... "
+zip -r ../dist/chrome/lb-imdb.zip *
+echo "Done"
+echo "Creating Firefox extension... "
+zip -r ../dist/firefox/lb-imdb.zip *
+echo "Done"
